9281cbb983009eac877db679e733cd5d801a32c6
Yuan Gabriel Lacambra <yuangabrielslacambra@iskolarngbayan.pup.edu.ph>
2022-06-29 14:54:55 +0800

    Starting Point (Fixed a fundamental flaw in my program, which is when you filter and then check the records of the remaining people on th elistbox, the record that will be shown is not their records but someone else's. Also changed some variable names to be more descriptive and accurate)

--- ContactTracingApp/ContactTracingInfoDatabaseForm.cs
+++ ContactTracingApp/ContactTracingInfoDatabaseForm.cs
@@ -19,7 +19,8 @@ namespace ContactTracingApp
         {
             InitializeComponent();
         }
-        List<string> CustomerInfoList = new List<string>();
+        List<string> CustomerNameList = new List<string>();
+        private bool FilterButtonClicked = false;
         private void ContactTracingInfoDatabaseForm_Load(object sender, EventArgs e)
         {
             int LineCount = 0;
@@ -31,28 +32,30 @@ namespace ContactTracingApp
                 if ((LineCount == 1 || ((LineCount - 1) % 10 == 0)))
                 {
                     ListBoxCustomerInfo.Items.Add(LineOnTheTextFile);
-                    CustomerInfoList.Add(LineOnTheTextFile);
+                    CustomerNameList.Add(LineOnTheTextFile);
                 }
 
             }
             ContactTracingInfoFileReader.Close();
+            FilterButtonClicked = false;
         }
+
         private void BtnCheckRecordOrBack_Click(object sender, EventArgs e)
         {
-            List<string> FirstCustomerInfoList = new List<string>();
             int ListBoxItemIndexNumber = ListBoxCustomerInfo.SelectedIndex;
             if (ListBoxCustomerInfo.SelectedIndex == -1 && BtnCheckRecordOrBack.Text == "Check Record")
             {
                 string NoneSelectedMsg = "You have not selected anything from the textbox. Please select an item.";
                 MessageBox.Show(NoneSelectedMsg, "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
-            if (ListBoxItemIndexNumber == 0 && BtnCheckRecordOrBack.Text == "Check Record")
+            if (ListBoxItemIndexNumber == 0 && BtnCheckRecordOrBack.Text == "Check Record" && FilterButtonClicked == false)
             {
                 ChangeBtnTextToBack();
                 int LineCount = 0;
                 StreamReader ContactTracingInfoFileReader = new StreamReader(@"C:\Users\Stefani\source\repos\ContactTracingApp\ContactTracingApp\bin\Debug\net6.0-windows\Contact Tracing Information from Customers.txt");
                 while (!ContactTracingInfoFileReader.EndOfStream)
                 {
+                    List<string> FirstCustomerInfoList = new List<string>();
                     string LineOnTheTextFile = ContactTracingInfoFileReader.ReadLine();
                     LineCount++;
                     if ((!(LineCount == 1 || ((LineCount - 1) % 10 == 0))) && LineCount < 11 && !(LineCount == 4))
@@ -67,7 +70,7 @@ namespace ContactTracingApp
                 }
                 ContactTracingInfoFileReader.Close();
             }
-            else if (ListBoxItemIndexNumber == 1 && BtnCheckRecordOrBack.Text == "Check Record")
+            else if (ListBoxItemIndexNumber == 1 && BtnCheckRecordOrBack.Text == "Check Record" && FilterButtonClicked == false)
             {
                 ChangeBtnTextToBack();
                 int LineCount = 0;
@@ -90,6 +93,7 @@ namespace ContactTracingApp
 
             else if (BtnCheckRecordOrBack.Text == "Back")
             {
+                FilterButtonClicked = false;
                 if (ListBoxCustomerInfo.Items.Count > 0)
                 {
                     ListBoxCustomerInfo.Items.Clear();
@@ -111,7 +115,7 @@ namespace ContactTracingApp
                     }
                 }
             }
-            else if (ListBoxItemIndexNumber > 1)
+            else if (ListBoxItemIndexNumber > 1 && FilterButtonClicked == false)
             {
                 ChangeBtnTextToBack();
                 int LineCount = 0;
@@ -133,7 +137,54 @@ namespace ContactTracingApp
                 }
                 ContactTracingInfoFileReader.Close();
             }
+            else if (FilterButtonClicked == true)
+            {
+                string ListBoxItemName = ListBoxCustomerInfo.GetItemText(ListBoxCustomerInfo.SelectedItem);
+                if (CustomerNameList.Contains(ListBoxItemName))
+                {
+                    ChangeBtnTextToBack();
+                    foreach (string CustomerNameWithDateVisited in CustomerNameList) 
+                    {
+                        if (CustomerNameWithDateVisited.Contains(ListBoxItemName))
+                        {
+                            int CustomerNameListItemIndex = CustomerNameList.IndexOf(CustomerNameWithDateVisited);
+                            if (CustomerNameListItemIndex == 0)
+                            {
+                                int LineCount = 0;
+                                StreamReader ContactTracingInfoFileReader = new StreamReader(@"C:\Users\Stefani\source\repos\ContactTracingApp\ContactTracingApp\bin\Debug\net6.0-windows\Contact Tracing Information from Customers.txt");
+                                while (!ContactTracingInfoFileReader.EndOfStream)
+                                {
+                                    string LineOnTheTextFile = ContactTracingInfoFileReader.ReadLine();
+                                    LineCount++;
+                                    if (LineCount > 1 && LineCount < 10)
+                                    {
+                                        ListBoxCustomerInfo.Items.Add(LineOnTheTextFile);
+                                    }
+                                }
+                                ContactTracingInfoFileReader.Close();
+                            }
+                            else if (CustomerNameListItemIndex > 0)
+                            {
+                                int LineCount = 0;
+                                StreamReader ContactTracingInfoFileReader = new StreamReader(@"C:\Users\Stefani\source\repos\ContactTracingApp\ContactTracingApp\bin\Debug\net6.0-windows\Contact Tracing Information from Customers.txt");
+                                while (!ContactTracingInfoFileReader.EndOfStream)
+                                {
+                                    string LineOnTheTextFile = ContactTracingInfoFileReader.ReadLine();
+                                    LineCount++;
+                                    if (LineCount > (CustomerNameListItemIndex * 10) && LineCount < ((CustomerNameListItemIndex * 10) + 10))
+                                    {
+                                        ListBoxCustomerInfo.Items.Add(LineOnTheTextFile);
+                                    }
+                                }
+                                ContactTracingInfoFileReader.Close();
+                            }
+
+                        }
+                    }
+                }
+            }
         }
+
         private void ChangeBtnTextToBack()
         {
             if (BtnCheckRecordOrBack.Text == "Check Record")
@@ -147,6 +198,7 @@ namespace ContactTracingApp
         }
         private void BtnFilter_Click(object sender, EventArgs e)
         {
+            FilterButtonClicked = true;
             ListBoxCustomerInfo.Items.Clear();
             if (TxtBoxSearchOrFilterDate.Text == "")
             {
@@ -163,11 +215,12 @@ namespace ContactTracingApp
                         ListBoxCustomerInfo.Items.Add(LineOnTheTextFile);
                     }
                 }
+                ContactTracingInfoFileReader.Close();
             }
-            foreach (string CustomerInfo in CustomerInfoList)
-                if (CustomerInfo.Contains(TxtBoxSearchOrFilterDate.Text))
+            foreach (string CustomerNameWithDateVisited in CustomerNameList)
+                if (CustomerNameWithDateVisited.Contains(TxtBoxSearchOrFilterDate.Text))
                 {
-                    ListBoxCustomerInfo.Items.Add(CustomerInfo);
+                    ListBoxCustomerInfo.Items.Add(CustomerNameWithDateVisited);
                 }
         }
     }
